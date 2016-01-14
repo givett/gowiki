190dd3da6d9929df6740c741446d65f85521da55
Gwen Ivett <givett@starkandwayne.com>
2016-01-14 14:46:22 -0500

    View and Edit added.

--- wiki.go
+++ wiki.go
@@ -5,6 +5,8 @@ package main
 import (
 	"fmt"
 	"io/ioutil"
+	"net/http"
+	"html/template"
 )
 
 // Page - define Page as a struct with two fields representing the title and body.
@@ -30,9 +32,47 @@ func loadPage(title string) (*Page, error) {
 	return &Page{Title: title, Body: body}, nil
 }
 
+func viewHandler(w http.ResponseWriter, r *http.Request) {
+	title := r.URL.Path[len("/view/"):]
+	p, _ := loadPage(title)
+	//fmt.Fprintf(w, "<h1>%s</h1><div>%s</div>", p.Title, p.Body)
+	//t, _ := template.ParsFiles("view.html")
+	//t.Execute(w, p)
+	renderTemplate(w, "view", p)
+}
+
+func editHandler(w http.ResponseWriter, r *http.Request) {
+	title := r.URL.Path[len("/edit/"):]
+	p, err := loadPage(title)
+	if err != nil {
+		p = &Page{Title: title}
+	}
+	//fmt.Fprintf(w, "<h1>Editing %s</h1>"+
+		//"<form action=\"/save/%s\" method=\"POST\">"+
+		//"<textarea name=\"body\">%s</textarea><br>"+
+		//"<input type=\"submit\" value=\"Save\">"+
+		//"</form>",
+		//p.Title, p.Title, p.Body)
+	//t, _ := template.ParseFiles("edit.html")
+	//t.Execute(w, p)
+	renderTemplate(w, "edit", p)
+}
+
+func renderTemplate(w http.ResponseWriter, tmpl string, p *Page) {
+	t, _ := template.ParseFiles(tmpl + ".html")
+	t.Execute(w, p)
+}
+
 func main() {
-	p1 := &Page{Title: "TestPage", Body: []byte("This is a sample Page.")}
-	p1.save()
-	p2, _ := loadPage("TestPage")
-	fmt.Println(string(p2.Body))
+  //First main function:
+	//p1 := &Page{Title: "TestPage", Body: []byte("This is a sample Page.")}
+	//p1.save()
+	//p2, _ := loadPage("TestPage")
+	//fmt.Println(string(p2.Body))
+
+	http.HandleFunc("/view/", viewHandler)
+	http.HandleFunc("/edit/", editHandler)
+	//http.HandleFunc("/save/", saveHandler)
+	fmt.Print("Listening on :8080")
+	http.ListenAndServe(":8080", nil)
 }
