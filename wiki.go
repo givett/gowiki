0f32bc5b1ca8afbd2464dea8b5afb5586186533a
Gwen Ivett <givett@starkandwayne.com>
2016-01-15 15:48:52 -0500

    Added saveHandler

--- wiki.go
+++ wiki.go
@@ -3,7 +3,7 @@
 package main
 
 import (
-	"fmt"
+	// "fmt"
 	"io/ioutil"
 	"net/http"
 	"html/template"
@@ -34,10 +34,17 @@ func loadPage(title string) (*Page, error) {
 
 func viewHandler(w http.ResponseWriter, r *http.Request) {
 	title := r.URL.Path[len("/view/"):]
-	p, _ := loadPage(title)
-	//fmt.Fprintf(w, "<h1>%s</h1><div>%s</div>", p.Title, p.Body)
-	//t, _ := template.ParsFiles("view.html")
-	//t.Execute(w, p)
+	p, err := loadPage(title)
+	if err != nil {
+		http.Redirect(w, r, "/edit/"+title, http.StausFound)
+		return
+	}
+
+	// fmt.Fprintf(w, "<h1>%s</h1><div>%s</div>", p.Title, p.Body)
+
+	// t, _ := template.ParsFiles("view.html")
+	// t.Execute(w, p)
+
 	renderTemplate(w, "view", p)
 }
 
@@ -47,14 +54,17 @@ func editHandler(w http.ResponseWriter, r *http.Request) {
 	if err != nil {
 		p = &Page{Title: title}
 	}
-	//fmt.Fprintf(w, "<h1>Editing %s</h1>"+
-		//"<form action=\"/save/%s\" method=\"POST\">"+
-		//"<textarea name=\"body\">%s</textarea><br>"+
-		//"<input type=\"submit\" value=\"Save\">"+
-		//"</form>",
-		//p.Title, p.Title, p.Body)
-	//t, _ := template.ParseFiles("edit.html")
-	//t.Execute(w, p)
+
+	// fmt.Fprintf(w, "<h1>Editing %s</h1>"+
+		// "<form action=\"/save/%s\" method=\"POST\">"+
+		// "<textarea name=\"body\">%s</textarea><br>"+
+		// "<input type=\"submit\" value=\"Save\">"+
+		// "</form>",
+		// p.Title, p.Title, p.Body)
+
+	// t, _ := template.ParseFiles("edit.html")
+	// t.Execute(w, p)
+
 	renderTemplate(w, "edit", p)
 }
 
@@ -63,16 +73,25 @@ func renderTemplate(w http.ResponseWriter, tmpl string, p *Page) {
 	t.Execute(w, p)
 }
 
+func saveHandler(w http.ResponseWriter, r *http.Request) {
+	title := r.URL.Path[len("/save/"):]
+	body := r.FormValue("body")
+	p:= &Page{Title: title, Body: []byte(body)}
+	p.save()
+	http.Redirect(w, r. "/view/"+title, http.StatusFound)
+}
+
 func main() {
-  //First main function:
-	//p1 := &Page{Title: "TestPage", Body: []byte("This is a sample Page.")}
-	//p1.save()
-	//p2, _ := loadPage("TestPage")
-	//fmt.Println(string(p2.Body))
+
+  // First main function:
+	// p1 := &Page{Title: "TestPage", Body: []byte("This is a sample Page.")}
+	// p1.save()
+	// p2, _ := loadPage("TestPage")
+	// fmt.Println(string(p2.Body))
 
 	http.HandleFunc("/view/", viewHandler)
 	http.HandleFunc("/edit/", editHandler)
-	//http.HandleFunc("/save/", saveHandler)
+	http.HandleFunc("/save/", saveHandler)
 	fmt.Print("Listening on :8080")
 	http.ListenAndServe(":8080", nil)
 }
